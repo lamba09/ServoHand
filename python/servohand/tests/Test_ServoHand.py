70b613e31a739ff2749ef19d3d91567fe46472bd
mario.seeli <mario.seeli@dectris.com>
2016-11-01 17:48:12 +0100

    added tests

--- python/servohand/tests/Test_ServoHand.py
+++ python/servohand/tests/Test_ServoHand.py
@@ -3,6 +3,11 @@ from servohand.ServoHand import ServoHand
 
 class Test_ServoHand:
 
+    def setup_method(self, method):
+        self.servohand = ServoHand("default")
+
+    def teardown_method(self, method):
+        del self.servohand
+
     def test_init(self):
         servohand = ServoHand("my_right_hand")
-        assert False
